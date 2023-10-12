dffc97495706280f5fcaca13a25497056b616813
jreyesve <jreyesve@my.centennialcollege.ca>
2023-10-12 12:47:14 -0400

    commit 2
    Add routes

---
+++ app/controllers/products.js
@@ -0,0 +1,119 @@
+let ProductModel = require('../models/product');
+
+/*Create product */
+module.exports.create = async function (req, res, next) {
+    try {
+        let newProduct = new ProductModel(req.body);
+
+        let result = await ProductModel.create(newProduct);
+        res.json(
+            {
+                success: true,
+                message: "Product created sucessfully."
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+}
+
+/*Search ALL Products */
+exports.list = async function (req, res, next) {
+    try {
+        let list = await ProductModel.find({}, '-password');
+        res.json(list);
+    } catch (error) {
+        next(error);
+    }
+}
+
+/*Search Product BY ID */
+exports.productByID = async function (req, res, next) {
+    try {
+        let productId = req.params.id;
+        req.user = await ProductModel.findOne({ _id: productId }, '-password');
+        next();
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+
+exports.read = function (req, res) {
+    res.json(req.user);
+};
+
+/*Update Product BY ID */
+exports.update = async (req, res, next) => {
+    try {
+        let productId = req.params.id;
+        let updatedProduct = ProductModel(req.body);
+        updatedProduct._id = productId;
+
+        let result = await ProductModel.updateOne({ _id: productId }, updatedProduct);
+        console.log(result);
+        if (result.modifiedCount > 0) {
+            res.json(
+                {
+                    success: true,
+                    message: "Product updated sucessfully."
+                }
+            );
+        }
+        else {
+            // Express will catch this on its own.
+            throw new Error('Product not updated. Are you sure it exists?')
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+/*Delete Product BY ID */
+module.exports.remove = async (req, res, next) => {
+     try {
+       let productId = req.params.id;
+       let result = await ProductModel.deleteOne({ _id: productId });
+       console.log("====> Result: ", result);
+       if (result.deletedCount > 0) {
+         res.json(
+           {
+             success: true,
+             message: "Product deleted sucessfully."
+           }
+         )
+       }
+       else {
+         // Express will catch this on its own.
+         throw new Error('User not deleted. Are you sure it exists?')
+       }
+     } catch (error) {
+       console.log(error);
+       next(error);
+     }
+    }
+    
+/*Delete ALL PRODUCTS */
+module.exports.remove = async (req, res, next) => {
+    try {
+      let productId = req.params.id;
+      let result = await ProductModel.deleteMany({});
+      console.log("====> Result: ", result);
+      if (result.deletedCount > 0) {
+        res.json(
+          {
+            success: true,
+            message: "All Products deleted sucessfully from the collection."
+          }
+        )
+      }
+      else {
+        // Express will catch this on its own.
+        throw new Error('There is not products in the collection to delete')
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+   }
--- config/app.js
+++ config/app.js
@@ -2,8 +2,10 @@ var createError = require('http-errors');
 var express = require('express');
 var logger = require('morgan');
 
+/*Defining Routes for every document */
 var indexRouter = require('../app/routes/index');
 var usersRouter = require('../app/routes/users');
+var productsRouter = require('../app/routes/products');/*<--- */
 
 var app = express();
 
@@ -13,6 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);/*<--- */
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
