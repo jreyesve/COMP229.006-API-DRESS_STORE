6af2f959fa5e9fc57d3a8255cd0e57aa92de85b9
jreyesve <jreyesve@my.centennialcollege.ca>
2023-10-13 22:30:18 -0400

    commit 4
    Last changes

--- config/app.js
+++ config/app.js
@@ -4,8 +4,7 @@ var logger = require('morgan');
 
 /*Defining Routes for every document */
 var indexRouter = require('../app/routes/index');
-var usersRouter = require('../app/routes/users');
-var productsRouter = require('../app/routes/products');/*<--- */
+var productsRouter = require('../app/routes/products');
 
 var app = express();
 
@@ -14,8 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);/*<--- */
+app.use('/api/products', productsRouter);
 
 
 // catch 404 and forward to error handler
